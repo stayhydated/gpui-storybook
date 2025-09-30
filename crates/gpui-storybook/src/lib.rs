89bf6805f67596460f04ce1c06338c2266fb6dda
FranzJvezNiceAxa <56778463+stayhydated@users.noreply.github.com>
2025-09-30 12:24:04 -0400

    Update lib.rs

--- crates/gpui-storybook/src/lib.rs
+++ crates/gpui-storybook/src/lib.rs
@@ -3,6 +3,7 @@ pub use gpui_storybook_macros::*;
 
 pub use gpui_storybook_core::assets::Assets;
 pub use gpui_storybook_core::gallery::Gallery;
+pub use gpui_storybook_core::i18n::change_locale;
 pub use gpui_storybook_core::story::{Story, StoryContainer, create_new_window, init};
 
 pub use gpui_storybook_core::registry as __registry;
