a53f92d581192d45e957675a4cb7fdb5eb112782
stayhydated <56778463+stayhydated@users.noreply.github.com>
2025-12-08 07:38:08 -0500

    basic tests

--- crates/gpui-storybook-macros/src/lib.rs
+++ crates/gpui-storybook-macros/src/lib.rs
@@ -1,15 +1,14 @@
 use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
-use syn::{ItemFn, ItemStruct, parse_macro_input};
+use syn::{ItemFn, ItemStruct};
 
-/// Attribute macro to register a story struct
-#[proc_macro_attribute]
-pub fn story(_args: TokenStream, input: TokenStream) -> TokenStream {
-    let input_struct = parse_macro_input!(input as ItemStruct);
+fn story_impl(_args: TokenStream2, input: TokenStream2) -> TokenStream2 {
+    let input_struct: ItemStruct = syn::parse2(input).expect("story macro expects a struct");
     let struct_name = &input_struct.ident;
     let struct_name_str = struct_name.to_string();
 
-    let expanded = quote! {
+    quote! {
         #input_struct
 
         gpui_storybook::__inventory::submit! {
@@ -20,18 +19,20 @@ pub fn story(_args: TokenStream, input: TokenStream) -> TokenStream {
                 },
             }
         }
-    };
-
-    expanded.into()
+    }
 }
 
-/// Attribute macro to register an init function
+/// Attribute macro to register a story struct
 #[proc_macro_attribute]
-pub fn story_init(_args: TokenStream, input: TokenStream) -> TokenStream {
-    let input_fn = parse_macro_input!(input as ItemFn);
+pub fn story(_args: TokenStream, input: TokenStream) -> TokenStream {
+    story_impl(_args.into(), input.into()).into()
+}
+
+fn story_init_impl(_args: TokenStream2, input: TokenStream2) -> TokenStream2 {
+    let input_fn: ItemFn = syn::parse2(input).expect("story_init macro expects a function");
     let fn_name = &input_fn.sig.ident;
 
-    let expanded = quote! {
+    quote! {
         #input_fn
 
         inventory::submit! {
@@ -39,7 +40,51 @@ pub fn story_init(_args: TokenStream, input: TokenStream) -> TokenStream {
                 init_fn: #fn_name,
             }
         }
-    };
+    }
+}
 
-    expanded.into()
+/// Attribute macro to register an init function
+#[proc_macro_attribute]
+pub fn story_init(_args: TokenStream, input: TokenStream) -> TokenStream {
+    story_init_impl(_args.into(), input.into()).into()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use insta::assert_snapshot;
+    use prettyplease::unparse;
+    use quote::quote;
+
+    fn snapshot_tokens(tokens: TokenStream2) -> String {
+        let file =
+            syn::parse2::<syn::File>(tokens).expect("generated code should be valid Rust syntax");
+        unparse(&file)
+    }
+
+    #[test]
+    fn story_generates_registry_entry() {
+        let input = quote! {
+            pub struct ButtonStory;
+        };
+
+        let expanded = story_impl(TokenStream2::new(), input);
+        assert_snapshot!(
+            "story_attribute_generates_registry_entry",
+            snapshot_tokens(expanded)
+        );
+    }
+
+    #[test]
+    fn story_init_generates_init_entry() {
+        let input = quote! {
+            pub fn setup() {}
+        };
+
+        let expanded = story_init_impl(TokenStream2::new(), input);
+        assert_snapshot!(
+            "story_init_attribute_generates_registry_entry",
+            snapshot_tokens(expanded)
+        );
+    }
 }
