0ab8b75a74dc2081b8f0d4ecee0551f47c6071a0
stayhydated <56778463+stayhydated@users.noreply.github.com>
2025-12-18 10:04:01 -0500

    Update main.rs

--- example/src/main.rs
+++ example/src/main.rs
@@ -17,6 +17,7 @@ fn main() {
     let name_arg = std::env::args().nth(1);
 
     app.run(move |app_cx| {
+        gpui_component::init(app_cx);
         gpui_storybook::init(Languages::default(), app_cx);
         gpui_storybook::change_locale(Languages::default());
 
